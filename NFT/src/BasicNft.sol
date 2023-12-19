c0999803d54898d3de04cfc3dbf049df6cf35eda
Zarina <113510101+ZarinaMye@users.noreply.github.com>
2023-12-19 22:26:55 +0100

    makefile

--- NFT/src/BasicNft.sol
+++ NFT/src/BasicNft.sol
@@ -8,18 +8,30 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
+    error BasicNft__TokenUriNotFound();
+
+    mapping(uint256 => string) private s_tokenIdToUri;
     uint256 private s_tokenCounter;
 
     constructor() ERC721("Horse", "HORSE") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return "";
-        //mha IPFS
+        if (ownerOf(tokenId) == address(0)) {
+            revert BasicNft__TokenUriNotFound();
+        }
+        return s_tokenIdToUri[tokenId];
+        //return "ipfs://http://QmNMtCYX8eGZcZb6v4AKtgaXM1LafBH4MFQYUi2GSmygBT"; //destributed decentralised network
+        //mha IPFS deploy
+        //i browser: http://ipfs.io/ipfs/bildens hash  //detta blir endast på en node
     }
 }
