2c1ddc950c7d16a75223ef2bd66166cfc28a9da4
Zarina <113510101+ZarinaMye@users.noreply.github.com>
2024-01-02 16:30:38 +0100

    deploy mha sepholia testnet

--- naramunz_contract/script/DeployDAO.s.sol
+++ naramunz_contract/script/DeployDAO.s.sol
@@ -1,1 +1,31 @@
 // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DAO} from "../src/DAO.sol";
+
+contract DeployDAO is Script {
+    function run() external returns (DAO) {
+        vm.startBroadcast(); //vm cheat code in Foundry, forge - std
+        DAO dao = new DAO();
+        vm.stopBroadcast();
+        return dao;
+    } //forge script script/DeployDAO.s.sol  för att testa filen på tillfällig url -anvil
+    // för att deploya mha anvil, ge RPC_URL o private key från anvil:
+    // anvil
+    // ny terminal:
+    // forge script script/DeployDAO.s.sol --rpc-url http://127.0.0.1:8545 --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+    /* function run() public {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // Deploy GovToken
+        GovToken govToken = new GovToken();
+
+        // Deploy MyGovernor
+        MyGovernor myGovernor = new MyGovernor(govToken, govToken);
+
+        vm.stopBroadcast();
+     */
+}
---
+++ naramunz_contract/test/TestDAO.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DAO} from "../src/DAO.sol";
+import {ERC721} from "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+/* contract DAOTest is Test {
+    DAO public dao;
+    ERC721 public erc721;
+    address constant OWNER = address(0x5E11E7);
+
+    struct Proposal {
+        string title;
+        string description;
+        uint256 yesVotes;
+        uint256 noVotes;
+    }
+
+    function setUp() public {
+        erc721 = new ERC721();
+        dao = new DAO();
+    }
+
+    function testCreateProposal() public {
+        erc721.mint(OWNER, 1);
+        dao.createProposal(
+            "Test Proposal",
+            "This is a test proposal",
+            erc721.address,
+            1
+        );
+
+        Proposal memory proposal = dao.getProposals(0);
+        assertEq(proposal.title, "Test Proposal");
+        assertEq(proposal.description, "This is a test proposal");
+    }
+
+    function testVote() public {
+        erc721.mint(OWNER, 2);
+        dao.createProposal(
+            "Test Proposal",
+            "This is a test proposal",
+            erc721.address,
+            1
+        );
+
+        dao.vote(0, erc721.address, 2, true);
+
+        Proposal memory proposal = dao.getProposals(0);
+        assertEq(proposal.yesVotes, 1);
+        assertEq(proposal.noVotes, 0);
+    }
+} */
+
+/* contract TestDAO is Test {
+    DAO dao;
+
+    function beforeEach() public {
+        dao = new DAO();
+    }
+
+    function testCreateProposal() public {
+        dao.createProposal("title", "description", address(this), 0);
+        DAO.Proposal memory proposal = dao.proposals(0);
+        assertEq(proposal.title, "title");
+        assertEq(proposal.description, "description");
+        assertEq(proposal.yesVotes, 0);
+        assertEq(proposal.noVotes, 0);
+    }
+
+    function testVote() public {
+        dao.createProposal("title", "description", address(this), 0);
+        dao.vote(0, address(this), 0, true);
+        DAO.Proposal memory proposal = dao.proposals(0);
+        assertEq(proposal.yesVotes, 1);
+        assertEq(proposal.noVotes, 0);
+    }
+
+    function testVoteFail() public {
+        dao.createProposal("title", "description", address(this), 0);
+        dao.vote(0, address(this), 0, true);
+        dao.vote(0, address(this), 0, true);
+        DAO.Proposal memory proposal = dao.proposals(0);
+        assertEq(proposal.yesVotes, 1);
+        assertEq(proposal.noVotes, 0);
+    }
+}
+ */
